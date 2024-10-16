5d122aaeb18175d563865ca94123d627c03383b2
Andrei <andreirech22@outlook.com>
2024-10-16 15:09:33 -0300

    feat: implement keyboard navigation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Pointer from '../../components/Pointer/index.tsx';
 import './index.scss';
 
 const Home: React.FC = () => {
 	const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+	const [selectedOption, setSelectedOption] = useState(false);
+	const navigate = useNavigate();
 	
   	const handleMouseMove = (event: React.MouseEvent) => {
 		setMousePosition({ x: event.clientX, y: event.clientY });
 	};
+
+	const handleKey = useCallback((event: KeyboardEvent) => {
+		const actions: { [key: string]: () => void } = {
+			'ArrowRight': () => handlePageChange('/options'),
+			'ArrowDown': () => setSelectedOption(true),
+			'ArrowUp': () => setSelectedOption(false),
+			'Enter': () => handlePageChange('/options'),
+		};
+
+		const action = actions[event.key];
+		if (action) action();
+	}, [navigate]);
+
+	useEffect(() => {
+		window.addEventListener('keydown', handleKey);
+		
+		return () => { 
+			window.removeEventListener('keydown', handleKey);
+		};
+	}, [handleKey]);
+
+	const handlePageChange = (page: string) => {
+        navigate(page);
+    };
 
 	return (
 		<div className='home-container' onMouseMove={handleMouseMove}>
@@ -19,7 +46,7 @@ const Home: React.FC = () => {
 						<p className='home-title'>welcome</p>
 					</div>
 					<div className='home-bottom'>
-						<p className='home-options'>options</p>
+						<p className={`home-options ${selectedOption ? 'selected' : ''}`} onClick={() => handlePageChange('/options')}>options</p>
 						<p className='home-navigate'>← use arrows to navigate →</p>
 					</div>
 				</div>
