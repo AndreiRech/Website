9f0017071b63c1761a76d147e0e9fefc76121027
Andrei <andreirech22@outlook.com>
2024-10-16 14:16:43 -0300

    feat: home page style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import Draggable from 'react-draggable';
+import React, { useState } from 'react';
 import Pointer from '../../components/Pointer/index.tsx';
 import './index.scss';
 
 const Home: React.FC = () => {
 	const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-	const [typedText, setTypedText] = useState('');
-
-	const text = 'WORK IN PROGRESS';
-	let index = 0;
-  
-	useEffect(() => {
-	  const typeText = () => {
-		if (index < text.length) {
-		  setTypedText(prevTypedText => prevTypedText + text.charAt(index));
-		  index++;
-		  setTimeout(typeText, 100);
-		}
-	  };
-  
-	  typeText();
-	}, []);
 	
   	const handleMouseMove = (event: React.MouseEvent) => {
 		setMousePosition({ x: event.clientX, y: event.clientY });
@@ -29,8 +12,17 @@ const Home: React.FC = () => {
 	return (
 		<div className='home-container' onMouseMove={handleMouseMove}>
 			<Pointer x={mousePosition.x} y={mousePosition.y} />
-			<div className='home-content'>
-				<Draggable><p className='home-wait'>{typedText}</p></Draggable>
+			<div className='home-border'>
+				<div className='home-content'>
+					<div className='home-main'>
+						<p className='home-title-jap'>いらしゃいませ</p>
+						<p className='home-title'>welcome</p>
+					</div>
+					<div className='home-bottom'>
+						<p className='home-options'>options</p>
+						<p className='home-navigate'>← use arrows to navigate →</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
