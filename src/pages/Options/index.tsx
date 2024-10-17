7c987d15646c7f432ffd988cd190bbc8c3eb6797
Andrei <andreirech22@outlook.com>
2024-10-17 15:57:09 -0300

    feat: added the svg

--- src/pages/Options/index.tsx
+++ src/pages/Options/index.tsx
@@ -4,6 +4,8 @@ import Pointer from '../../components/Pointer/index.tsx';
 import './index.scss';
 import Box from '../../components/Box/index.tsx';
 import Card from '../../components/Card/index.tsx';
+import { ReactComponent as Moon } from '../../assets/icons/moon.svg';
+import { ReactComponent as Sun } from '../../assets/icons/sun.svg';
 
 const Options: React.FC = () => {
 	const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -35,71 +37,76 @@ const Options: React.FC = () => {
 	return (
 		<div className='option-container' onMouseMove={handleMouseMove}>
 			<Pointer x={mousePosition.x} y={mousePosition.y} />
-			<Box color='grey'></Box>
-			<div className='option-content'>
-				<div className='option-top'>
-					<Card color='grey'>
-						<div className='card-options'>
-							<p className='card-title-jap'>オプション</p>
-							<p className='card-title'>options</p>
-						</div>
-					</Card>
-					<Card color='grey'>
-						<div className='card-restore'>
-							<p className='card-title-jap'>復元</p>
-							<p className='card-title'>restore settings</p>
-						</div>
-					</Card>
-					<Card color='grey'>
-						<div className='card-exit'>
-							<p className='card-title-jap'>出かける</p>
-							<p className='card-title'>exit</p>
-						</div>
-					</Card>
-				</div>
-				<div className='option-bottom'>
-
-					<div className='option-colors'>
+			<Box color='grey'>
+				<div className='option-content'>
+					<div className='option-top'>
 						<Card color='grey'>
-							<div className='card-color'>
-								<div className='card-color-top'>
-									<p className='card-color-title'>color</p>
-									<p className='card-text'>bright</p>
-								</div>
-								<div className='card-color-bottom'>
-									<p className='card-color-jap'>ライトモード</p>
-								</div>
+							<div className='card-options'>
+								<p className='card-title-jap'>オプション</p>
+								<p className='card-title'>options</p>
 							</div>
 						</Card>
 						<Card color='grey'>
-						<div className='card-background'>
-							<div className='card-background-top'>
-								<p className='card-background-title'>background</p>
-								<p className='card-text'>dark</p>
+							<div className='card-restore'>
+								<p className='card-title-jap-res'>復元</p>
+								<p className='card-title-res'>restore settings</p>
 							</div>
-							<div className='card-background-bottom'>
-								<p className='card-background-jap'>ダークモード</p>
+						</Card>
+						<Card color='grey'>
+							<div className='card-exit'>
+								<p className='card-title-jap-ex'>出かける</p>
+								<p className='card-title-ex'>exit</p>
 							</div>
-						</div>
-					</Card>
+						</Card>
 					</div>
-
-					<Card color='grey'>
-						<div className='card-language'>
-							<div className='card-language-title'>
-								<p className='card-title-jap'>言語</p>
-								<p className='card-title'>language</p>
-							</div>
-							<div className='card-language-body'>
-								<p className='card-text'>english</p>
-								<p className='card-text'>portugues</p>
-								<p className='card-text'>espanol</p>
-								<p className='card-text'>日本語</p>
+					<div className='option-bottom'>
+						<div className='option-colors'>
+							<div className='option-color-box'>
+								<Card color='grey'>
+									<div className='card-color'>
+										<div className='card-color-top'>
+											<p className='card-color-title'>color</p>
+											<p className='card-text'>bright</p>
+										</div>
+										<div className='card-color-bottom'>
+											<Sun className='svg-sun'/>
+											<p className='card-color-jap'>ライトモード</p>
+										</div>
+									</div>
+								</Card>
+							</div>	
+							<div className='option-background-box'>
+								<Card color='grey'>
+									<div className='card-background'>
+										<div className='card-background-top'>
+											<p className='card-background-title'>background</p>
+											<p className='card-text'>dark</p>
+										</div>
+										<div className='card-background-bottom'>
+											<Moon className='svg-moon'/>
+											<p className='card-background-jap'>ダークモード</p>
+										</div>
+									</div>
+								</Card>
 							</div>
 						</div>
-					</Card>
+						<Card color='grey'>
+							<div className='card-language'>
+								<div className='card-language-title'>
+									<p className='card-title-jap'>言語</p>
+									<p className='card-title'>language</p>
+								</div>
+								<div className='card-language-body'>
+									<p className='card-text'>english</p>
+									<p className='card-text'>portugues</p>
+									<p className='card-text'>espanol</p>
+									<p className='card-text'>日本語</p>
+								</div>
+							</div>
+						</Card>
+					</div>
 				</div>
-			</div>
+			</Box>
 		</div>
 	);
 }
